2015d3ae68bebfe12d5f8d6fd0158aba9878e32e
abril099 <70781953+abril099@users.noreply.github.com>
2020-09-19 19:27:40 -0300

    Update CuentaBic.cpp

--- CuentaBic.cpp
+++ CuentaBic.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 struct Tarjeta {
-	char CuentaID[10];
+	int  CuentaID;
 	int FechaCreacion;
 	bool Activa;
 	int Saldo;
@@ -19,54 +19,129 @@ struct Movimiento {
 	int CuentaID;
 };
 
-void ListarUsuarios(); 
-void AgregarUsuario();
-int EliminarUsuario(char Cuenta[]);
-int BuscarUsuario(char Cuenta[]);
+void Menu(int opcion);
+void LeerTarjetas();
+void AgregarTarjetas();
+void EliminarUsuario();
+void BuscarUsuario();
+void ListadoTarjetasAct();
+void CargarMovimiento();
+void LeerMovimientos();
+int cantidad();
+int CantidadMov();
+void Actualizacion();
+void SoloActivos();
+
 int  main () {
-	FILE*archivo;
-	int opcion = 0;
-	while(opcion!=6) {
-	cout<<" 1 - Listado de usuarios "<< endl;
-	cout<<" 2 - Agregar un nuevo usuario "<< endl;
-	cout<<" 3 - Eliminar una cuenta (por id) "<< endl;
-	cout<<" 4 - Buscar por id de la cuenta y mostrar "<< endl;
-	cout<<" 5 - Listar cuentas activas por saldo descendente "<< endl;
-	cout<<" 6 - Salir y guardar cambios "<< endl;
-	cout<<" Elija una opcion: "<< endl;
+	int opcion;
+	Menu(opcion);
+	return 0;
+	}
+
+
+void Menu(int opcion){
+
+    cout<<"Menu"<<endl;
+	cout<<"----------------------------------------------"<<endl;
+	cout<<"Opcion 1: Ingresar una tarjeta"<< endl;
+	cout<<"Opcion 2: Muestra las tarjetas ingresadas"<< endl;
+	cout<<"Opcion 3: Eliminar una cuenta (por id) "<< endl;
+	cout<<"Opcion 4: Buscar por id de la cuenta y mostrar "<< endl;
+	cout<<"Opcion 5: Listado de las cuentas activas por saldo descendente"<< endl;
+	cout<<"Opcion 6: Ingresar un movimiento"<<endl;
+	cout<<"Opcion 7: Muestra los movimientos ingresados"<<endl;
+	cout<<"Opcion 8: Actualizacion de las cuentas"<<endl;
+	//cout<<" 6 - Salir y guardar cambios "<< endl;
+    cout<<endl;
+    cout<<" Elija una opcion: "<< endl;
 	cin >>opcion;
-	char cuentaBuscada[10];
+	cout<<endl;
+
+
+
+
 	switch (opcion){
 		case 1:
-			 ListarUsuarios();
+			AgregarTarjetas();
+			Menu(opcion);
 			break;
-		case 2:	
-			AgregarUsuario();
+		case 2:
+			LeerTarjetas();
+			Menu(opcion);
 			break;
 		case 3:
-			cout<<"Ingrese el numero de Cuenta que desea borrar: "<<endl;
-			cin>>cuentaBuscada;
-			EliminarUsuario(cuentaBuscada);
+			EliminarUsuario();
+			Menu(opcion);
 			break;
 		case 4:
-			cout<<"Ingrese el numero de Cuenta que desea buscar: "<<endl;
-			cin>>cuentaBuscada;
-			BuscarUsuario(cuentaBuscada);
+		   BuscarUsuario();
+           Menu(opcion);
 			break;
-		case 5:	
+		case 5:
+		    ListadoTarjetasAct();
+		    Menu(opcion);
 			break;
 		case 6:
+        CargarMovimiento();
+         Menu(opcion);
 			break;
-		default:
-			break;		
-	}
-	cout <<" ----------- "<< endl;
-	}
-	fclose (archivo);
-	return 0;
+        case 7:
+         LeerMovimientos();
+            Menu(opcion);
+            break;
+        case 8:
+            Actualizacion();
+            Menu(opcion);
+            break;
+        case 9:
+            SoloActivos();
+            break;
+
 }
+}
+
+
+
+void LeerTarjetas() {
+	FILE*archivo;
+	archivo=fopen("Cuentas.BIC","rb");
+	Tarjeta t;
+	while(fread(&t,sizeof(Tarjeta),1,archivo)){
+    cout<<"Numero de Cuenta: "<<t.CuentaID<<endl;
+    cout<<"Fecha de creacion: "<<t.FechaCreacion<<endl;
+	if(t.Activa){
+		 cout<<"Cuenta Activa: Si"<<endl;
+	}else{
+		cout<<"Cuenta Activa: No"<<endl;
+	}
+    cout<<"El saldo de la tarjeta es: "<<t.Saldo<<endl;
+    cout<<"El numero de cliente es: "<<t.NroCliente<<endl;
+    cout<<"-------------------------------"<<endl;
+	}
+		fclose(archivo);
+	}
+
+void AgregarTarjetas(){
+	FILE*archivo;
+	archivo=fopen("Cuentas.BIC","ab");
+	Tarjeta t;
+	cout<<"Ingrese el numero de la tarjeta:"<<endl;
+	cin>>t.CuentaID;
+	cout<<"Ingrese su Fecha de Creacion:"<<endl;
+	cin>>t.FechaCreacion;
+	cout<<"Ingrese estado de la tarjeta: (True=Activa o 1 o False=Desactivada o 0)"<<endl;
+	cin>>t.Activa;
+	cout<<"Ingrese el saldo de la cuenta:"<<endl;
+	cin>>t.Saldo;
+	cout<<"Ingrese el numero de cliente:"<<endl;
+	cin>>t.NroCliente;
+
+	fwrite(&t,sizeof(Tarjeta),1,archivo);
+	fclose(archivo);
+}
+
 int cantidad(){
-FILE* archivo; 
+FILE* archivo;
 archivo=fopen("Cuentas.BIC","rb");
 Tarjeta t;
 int cant=0;
@@ -77,18 +152,99 @@ fclose(archivo);
 return cant;
 }
 
-void ordenar(){
+void EliminarUsuario(){
+	FILE*archivo;
+	archivo=fopen("Cuentas.BIC","rb");
+	Tarjeta t;
+	Tarjeta arr[cantidad()];
+  int p=0;
+ while(fread(&t,sizeof(Tarjeta),1,archivo)){
+   arr[p].Activa=t.Activa;
+   arr[p].FechaCreacion=t.FechaCreacion;
+   arr[p].NroCliente=t.NroCliente;
+   arr[p].Saldo=t.Saldo;
+   arr[p].CuentaID=t.CuentaID;
+   p++;
+}
+fclose(archivo);
+FILE* otro;
+otro=fopen("otro.dat","ab");
+Tarjeta k;
+int eliminar;
+
+cout<<"Ingrese en numero de cuenta que desea eliminar"<<endl;
+cin>>eliminar;
+
+  for(int l=0;l<cantidad();l++){
+        if(arr[l].CuentaID!=eliminar){
+
+       k.Activa=arr[l].Activa;
+       k.FechaCreacion=arr[l].FechaCreacion;
+       k.NroCliente=arr[l].NroCliente;
+       k.Saldo=arr[l].Saldo;
+       k.CuentaID=arr[l].CuentaID;
+
+    fwrite(&k,sizeof(Tarjeta),1,otro);
+            }
+            l++;
+		}
+    fclose(otro);
+        remove("Cuentas.BIC");
+        rename("otro.dat","Cuentas.BIC");
+
+	}
+
+void BuscarUsuario(){
+	FILE*archivo;
+	archivo = fopen("Cuentas.BIC","rb");
+	Tarjeta t;
+	int buscar;
+	bool existe=false;
+	cout<<"Ingrese el id de la cuenta a buscar:"<<endl;
+	cin>>buscar;
+
+
+		while(fread(&t,sizeof(Tarjeta),1,archivo)){
+			if(t.CuentaID==buscar){
+                cout << "****** Datos de la cuenta *******" << endl;
+                cout << "CuentaID: " << t.CuentaID << endl;
+                cout << "Fecha de Creacion: " << t.FechaCreacion << endl;
+                cout << "Saldo: " << t.Saldo << endl;
+                cout << "Nro de Cliente: " << t.NroCliente << endl;
+                if(t.Activa){
+                	cout << "Activa: Si"<< endl;
+				}else{
+					cout << "Activa: No"<< endl;
+				}
+				existe=true;
+
+			}
+		}
+
+        if(!existe){
+            cout<<endl;
+            cout<<"El usuario no existe"<<endl;
+            cout<<endl;
+        }
+
+    fclose(archivo);
+}
+
+
+
+void ListadoTarjetasAct(){
 FILE* archivo;
 archivo=fopen("Cuentas.BIC","rb");
 Tarjeta t;
 Tarjeta array[cantidad()];
 int c=0;
+
 while(fread(&t,sizeof(Tarjeta),1,archivo)){
 array[c].Activa=t.Activa;
 array[c].FechaCreacion=t.FechaCreacion;
 array[c].NroCliente=t.NroCliente;
 array[c].Saldo=t.Saldo;
-array[c].TarjetaID=t.TarjetaID;
+array[c].CuentaID=t.CuentaID;
 c++;
 }
 
@@ -103,129 +259,159 @@ aux.Saldo=array[j].Saldo;
 aux.Activa=array[j].Activa;
 aux.FechaCreacion=array[j].FechaCreacion;
 aux.NroCliente=array[j].NroCliente;
-aux.TarjetaID=array[j].TarjetaID;
+aux.CuentaID=array[j].CuentaID;
 array[j].Saldo=array[j+1].Saldo;
 array[j].FechaCreacion=array[j+1].FechaCreacion;
 array[j].NroCliente=array[j+1].NroCliente;
 array[j].Activa=array[j+1].Activa;
-array[j].TarjetaID=array[j+1].TarjetaID;
+array[j].CuentaID=array[j+1].CuentaID;
 array[j+1].Saldo=aux.Saldo;
 array[j+1].Activa=aux.Activa;
 array[j+1].FechaCreacion=aux.FechaCreacion;
 array[j+1].NroCliente=aux.NroCliente;
-array[j+1].TarjetaID=aux.TarjetaID;
+array[j+1].CuentaID=aux.CuentaID;
 
 }
 }
 }
 
-FILE* arch;
-arch=fopen("a.dat","ab");
-Tarjeta au;
-for(int f=0;f<cantidad();f++){
-        au.Activa=array[f].Activa;
-        au.FechaCreacion=array[f].FechaCreacion;
-        au.NroCliente=array[f].NroCliente;
-        au.Saldo=array[f].Saldo;
-        au.TarjetaID=array[f].TarjetaID;
-    fwrite(&au,sizeof(Tarjeta),1,arch);
+for(int m=0;m<cantidad();m++){
+if(array[m].Activa){
+    cout<<"Numero de Cuenta: "<<array[m].CuentaID<<endl;
+    cout<<"Fecha de creacion: "<<array[m].FechaCreacion<<endl;
+	cout<<"Cuenta Activa: Si"<<endl;
+    cout<<"El saldo de la tarjeta es: "<<array[m].Saldo<<endl;
+    cout<<"El numero de cliente es: "<<array[m].NroCliente<<endl;
+    cout<<"-------------------------------"<<endl;
+    }
 }
-fclose(arch);
+
+}
+
+void CargarMovimiento(){
+FILE* mov;
+FILE* sol;
+sol=fopen("sol.dat","ab"),
+mov=fopen("procesados.BIC","ab");
+Movimiento m;
+Movimiento s;
+cout<<"Ingrese el numero de movimiento:"<<endl;
+cin>>m.MovimientoID;
+cout<<"Ingrese la Fecha y Hora : (AAAAMMDDHH:MM)"<<endl;
+cin>>m.FechaHora;
+cout<<"Ingrese el monto:"<<endl;
+cin>>m.Monto;
+s.Monto=m.Monto;
+cout<<"Ingrese el numero de cuenta:"<<endl;
+cin>>m.CuentaID;
+s.CuentaID=m.CuentaID;
+
+fwrite(&s,sizeof(Movimiento),1,sol);
+fwrite(&m,sizeof(Movimiento),1,mov);
+fclose(mov);
+fclose(sol);
+}
+
+void LeerMovimientos(){
+FILE* mov;
+
+mov=fopen("procesados.BIC","rb");
+Movimiento m;
+
+
+ while(fread(&m,sizeof(Movimiento),1,mov)){
+cout<<"-------------------------------"<<endl;
+cout<<"El ID del movimiento es:"<<endl;
+cout<<m.MovimientoID<<endl;
+
+cout<<"La fecha y hora es:"<<endl;
+cout<<m.FechaHora<<endl;
+cout<<"El monto es:"<<endl;
+cout<<m.Monto<<endl;
+cout<<"El ID de la cuenta es:"<<endl;
+cout<<m.CuentaID<<endl;
+cout<<"-------------------------------"<<endl;
+}
+
+fclose(mov);
+}
+
+int CantidadMov(){
+FILE* mov;
+mov=fopen("procesados.BIC","rb");
+Movimiento m;
+int cont=0;
+while(fread(&m,sizeof(Movimiento),1,mov)){
+cont++;
+}
+fclose(mov);
+return cont;
+}
+
+
+void Actualizacion(){
+
+FILE* sol;
+sol=fopen("sol.dat","rb");
+
+Movimiento s;
+Movimiento cp[CantidadMov()];
+int h=0;
+
+while(fread(&s,sizeof(Movimiento),1,sol)){
+cp[h].CuentaID=s.CuentaID;
+cp[h].Monto=s.Monto;
+h++;
+}
+fclose(sol);
+FILE* archivo;
+archivo=fopen("Cuentas.BIC","rb");
+Tarjeta t;
+
+int suma[cantidad()];
+int suna;
+int g=0;
+while(fread(&t,sizeof(Tarjeta),1,archivo)){
+
+suna=t.Saldo;
+for(int a=0;a<CantidadMov();a++){
+    if(t.CuentaID==cp[a].CuentaID){
+    suna=suna+cp[a].Monto;
+   }
+}
+suma[g]=suna;
+g++;
+}
+fclose(archivo);
+FILE* luna;
+luna=fopen("luna.dat","ab");
+archivo=fopen("Cuentas.BIC","rb");
+Tarjeta c;
+int x=0;
+while(fread(&t,sizeof(Tarjeta),1,archivo)){
+c.Activa=t.Activa;
+c.CuentaID=t.CuentaID;
+c.FechaCreacion=t.FechaCreacion;
+c.NroCliente=t.NroCliente;
+
+for(int e=0;e<CantidadMov();e++){
+if(t.CuentaID==cp[e].CuentaID){
+c.Saldo=suma[x];
+}else{
+c.Saldo=t.Saldo;
+}
+}
+fwrite(&c,sizeof(Tarjeta),1,luna);
+x++;
+}
+fclose(archivo);
+fclose(luna);
+
 remove("Cuentas.BIC");
-rename("a.dat","Cuentas.BIC");
+rename("luna.dat","Cuentas.BIC");
+
 }
 
-void ListarUsuarios () {
-        ordenar();
-	FILE*archivo;
-	archivo=fopen("Cuentas.BIC","rb");
-	Tarjeta t;
-
-	while(fread(&t,sizeof(Tarjeta),1,archivo)){
-		cout<<"Numero de Cuenta: "<<t.CuentaID<<endl;
-		cout<<"Fecha de creacion: "<<t.FechaCreacion<<endl;
-	if(t.Activa){
-		 cout<<"Cuenta Activa: Si"<<endl;
-	}else{
-		cout<<"Cuenta Activa: No"<<endl;
-	}
-		cout<<"El saldo de la tarjeta es: "<<t.Saldo<<endl;
-		cout<<"El numero de cliente es: "<<t.NroCliente<<endl;
-		cout<<"-------------------------------"<<endl;
-	}
-		fclose(archivo);
-	} 
-
-void AgregarUsuario(){
-
-	FILE*archivo;
-	archivo=fopen("Cuentas.BIC","ab");
-	Tarjeta t;
-	cout<<"Ingrese el numero de la tarjeta:"<<endl;
-	cin>>t.CuentaID;
-	cout<<"Ingrese su Fecha de Creacion:"<<endl;
-	cin>>t.FechaCreacion;
-	cout<<"Ingrese estado de la tarjeta: (True=Activa o 1 o False=Desactivada o 0)"<<endl;
-	cin>>t.Activa;
-	cout<<"Ingrese el saldo de la cuenta:"<<endl;
-	cin>>t.Saldo;
-	cout<<"Ingrese el numero de cliente:"<<endl;
-	cin>>t.NroCliente;
-	fwrite(&t,sizeof(Tarjeta),1,archivo);
-	fclose(archivo);
-}
-
-int EliminarUsuario(char Cuenta[]){
-	FILE*archivo;
-	int encontrado = 0;
-	Tarjeta t;
-	if(archivo = fopen("Cuentas.BIC","rb+")){
-		while(!encontrado && fread(&t,sizeof(Tarjeta),1,archivo)){
-			if(strcmp(Cuenta,t.CuentaID)==0){
-				encontrado=1;
-				t.Activa=false;
-				fseek(archivo,sizeof(Tarjeta),SEEK_CUR);
-				fwrite(&t,sizeof(Tarjeta),1,archivo);
-			}
-		}
-		if(!encontrado){
-			cout << "No se encontro la persona a borrar." << endl;
-		}
-		fclose(archivo);
-    	return 1;
-	}
-	return 0;
-}
-
-int BuscarUsuario(char Cuenta[]){
-	FILE*archivo;
-	int encontrado = 0;
-	Tarjeta t;
-	if(archivo = fopen("Cuentas.BIC","rb+")){
-		while(!encontrado && fread(&t,sizeof(Tarjeta),1,archivo)){
-			if(strcmp(Cuenta,t.CuentaID)==0){
-				encontrado = 1;
-                cout << "****** Datos de la cuenta *******" << endl;
-                cout << "CuentaID: " << t.CuentaID << endl;
-                cout << "Fecha de Creacion: " << t.FechaCreacion << endl;
-                cout << "Saldo: " << t.Saldo << endl;
-                cout << "Nro de Cliente: " << t.NroCliente << endl;
-                if(t.Activa){
-                	cout << "Activa: Si"<< endl;
-				}else{
-					cout << "Activa: No"<< endl;
-				}
-				fwrite(&t,sizeof(Tarjeta),1,archivo);
-			}
-		}
-		if(!encontrado){
-			cout << "No se encontro la persona a borrar." << endl;
-		}
-		fclose(archivo);
-    	return 1;
-	}
-	return 0;
-}
 
 void SoloActivos(){
 FILE* archivo;
@@ -238,23 +424,23 @@ areglo[u].Activa=t.Activa;
 areglo[u].FechaCreacion=t.FechaCreacion;
 areglo[u].NroCliente=t.NroCliente;
 areglo[u].Saldo=t.Saldo;
-areglo[u].TarjetaID=t.TarjetaID;
+areglo[u].CuentaID=t.CuentaID;
 u++;
 }
 fclose(archivo);
 FILE* copia;
 copia=fopen("copia.dat","ab");
 
-Tarjeta c;
+Tarjeta v;
 
 for(int r=0;r<cantidad();r++){
 if(areglo[r].Activa){
-c.Activa=areglo[r].Activa;
-c.FechaCreacion=areglo[r].FechaCreacion;
-c.NroCliente=areglo[r].NroCliente;
-c.Saldo=areglo[r].Saldo;
-c.TarjetaID=areglo[r].TarjetaID;
-fwrite(&c,sizeof(Tarjeta),1,copia);
+v.Activa=areglo[r].Activa;
+v.FechaCreacion=areglo[r].FechaCreacion;
+v.NroCliente=areglo[r].NroCliente;
+v.Saldo=areglo[r].Saldo;
+v.CuentaID=areglo[r].CuentaID;
+fwrite(&v,sizeof(Tarjeta),1,copia);
 }
 }
 fclose(copia);
@@ -262,44 +448,4 @@ fclose(copia);
 remove("Cuentas.BIC");
 rename("copia.dat","Cuentas.BIC");
 
-
-
-}
-
-
-void CargarMovimiento(){
-FILE* mov;
-mov=fopen("procesados.BIC","ab");
-Movimiento m;
-cout<<"Ingrese el numero de movimiento:"<<endl;
-cin>>m.MovimientoID;
-cout<<"Ingrese la Fecha y Hora : (AAAAMMDDHH:MM)"<<endl;
-cin>>m.FechaHora;
-cout<<"Ingrese el monto:"<<endl;
-cin>>m.Monto;
-cout<<"Ingrese el numero de cuenta:"<<endl;
-cin>>m.CuentaID;
-
-fwrite(&m,sizeof(Movimiento),1,mov);
-fclose(mov);
-}
-
-
-void LeerMovimientos(){
-FILE* mov;
-mov=fopen("procesados.BIC","rb");
-Movimiento m;
- while(fread(&m,sizeof(Movimiento),1,mov)){
-cout<<"-------------------------------"<<endl;
-cout<<"El ID del movimiento es:"<<endl;
-cout<<m.MovimientoID<<endl;
-cout<<"La fecha y hora es:"<<endl;
-cout<<m.FechaHora<<endl;
-cout<<"El monto es:"<<endl;
-cout<<m.Monto<<endl;
-cout<<"El ID de la cuenta es:"<<endl;
-cout<<m.CuentaID<<endl;
-cout<<"-------------------------------"<<endl;
-}
-fclose(mov);
 }
